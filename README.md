cc37244160d89bf8e374094d5c76a920f6ae2ef8
SextantAI-Aleksandr <aleksandr@sextant-ai.com>
2023-02-14 06:50:30 -0500

    MVP

--- README.md
+++ README.md
@@ -1,51 +1,47 @@
 # WikiEnGine
 
+The WikiEnGine library is intended to make it easy to read and parse Wikipedia articles. It uses the CirrusSearch dump files located [here](https://dumps.wikimedia.org/other/cirrussearch/).
+
+**NOTE**: This file has been tested with the recent [enwiki-20230206-cirrussearch-content.json.gz](https://dumps.wikimedia.org/other/cirrussearch/20230206/enwiki-20230206-cirrussearch-content.json.gz) file. Other dates and languages have not been examined.
+
+
+
+
 ## Quickstart 
 
 ```bash
-# set the directory where you want to save the wiki dump files
-export WIKI_DIR="/path/to/download/directory" 
+# Download the CirrusSearch wiki dump:
+wget https://dumps.wikimedia.org/other/cirrussearch/20230206/enwiki-20230206-cirrussearch-content.json.gz
 
-# Download the file for a selected year-month-date: 
-# See https://dumps.wikimedia.org/other/cirrussearch/
-./download.sh YYYYMMDD
+# Unzip the file (unzips to ~150 GB)
+gzip -dk enwiki*.json.gz
 
-# Incude the py folder in your PYTHONPATH
-export PYTHONPATH="$PWD/py:$PYTHONPATH"
+# Clone this repository if you haven't already:
+git clone https://github.com/SextantAI-Aleksandr/WikiEnGine
+
+# Incude the WikiEnGine/py folder in your PYTHONPATH
+export PYTHONPATH="/path/to/WikiEnGine/py:$PYTHONPATH"
 ```
 
-Ensure the file is downloaded and PYTHONPATH has been set. You can then import and use the library like this:
+Once the dump has been downloaded and unzipped and PYTHONPATH has been set, you can import WikiEnGine like this:
 
 ```python
 from WikiEnGine import WikiEngine, Article
-
-wen = WikiEngine("/path/to/enwiki-YYYYMMDD-cirrussearch-content.json")
-while True:
-    article: Article = wen.next()
-    
+wen = WikiEngine('/path/to/enwiki-20230206-cirrussearch-content.json')
+for article in wen.generate(): # the .generate() method produces a generator
+    print(article.title)
+    print(article.text)
 ```
 
 
 
-## Background and Other resources
+## What other properties are available for articles?
 
-Wikipedia is a treasure trove of useful information on a wide variety of topics. There are many different download formats available and different tools for extracting/parsing those. This can make picking an approach difficult. 
+Look at the definition of the Article class [here](https://github.com/SextantAI-Aleksandr/WikiEnGine/blob/main/py/WikiEnGine.py).
 
-[This wiki page](https://en.wikipedia.org/wiki/Wikipedia:Database_download) has a nice description of the varios download files and formats. It says you probably want a file like the one below available from [this page](https://dumps.wikimedia.org/enwiki/)
 
-[https://dumps.wikimedia.org/enwiki/20230201/enwiki-20230201-pages-articles-multistream.xml.bz2](https://dumps.wikimedia.org/enwiki/20230201/enwiki-20230201-pages-articles-multistream.xml.bz2)
 
-But is that really the best choice for your use case?
+## What other dates are available?:
 
-### Libraries for parsing  enwiki-YYYYMMDD-pages-articles-multistream.xml.bz2 files 
-
-#### WikiExtractor 
-
-The most common library seems to be WikiExtractor (https://github.com/attardi/wikiextractor). It works well for most articles: The text is reasonably clean and preserves paragraphs "\n" and [[links]] to referenced articles. But a surpirsing number of articles have text:"" which is clearly incorrect.
-
-Note this repository also contains functionality for parsing wiki cirrus files (https://dumps.wikimedia.org/other/cirrussearch/20230206/). 
-
-#### PlainTextWikiExtractor 
-
-The PlainTextWikiExtractor (https://github.com/daveshap/PlainTextWikipedia) does not preserve paragraphs or links. It is build on top of WikiTextParse (https://pypi.org/project/wikitextparser/). It dumps each and every article as a id.json file, which makes for a large directory but does help identify them. 
+See the list of dates for CirrusSearch dumps here: [https://dumps.wikimedia.org/other/cirrussearch/](https://dumps.wikimedia.org/other/cirrussearch/).
 
--- py/WikiEnGine.py
+++ py/WikiEnGine.py
@@ -1,4 +1,15 @@
-from typing import Dict, Optional
+# This library can aid in parsing articles from Wikipedia Cirrus dumps
+#
+# EXAMPLE USAGE:
+# from WikiEnGine import WikiEngine
+# wen = WikiEngine('/path/to/enwiki-20230206-cirrussearch-content.json')
+# for article in wen.generate():
+#     print(article.title)
+#     print(article.text)
+# 
+
+from typing import Dict, List, Optional
+from datetime import datetime
 import json 
 
 
@@ -7,10 +18,37 @@ def row_json(row: str) -> Dict[str, object]:
     return dict(jz) 
 
 
+class Place:
+    def __init__(self, coord: Dict[str, object]):
+        # The 'coordinates' field seems to give a list of JSON objects, commonly like this:
+        # {'country': 'US', 'coord': {'lon': -112.47583333333334, 'lat': 33.513888888888886}, 'globe': 'earth', 'name': None, 'dim': 30000, 'type': 'adm2nd', 'region': 'AZ', 'primary': True}
+        # This class captures the most commonly populated elements 
+        self.country: Optional[str] = coord['country']
+        self.region: Optional[str] = coord['region']
+        self.lat: float = float(coord['coord']['lat'])
+        self.lon: float = float(coord['coord']['lon'])
+
+
 class Article:
     def __init__(self, jz: Dict[str, object]):
+        # required properties
+        self.page_id: int = int(jz['page_id'])
+        self.version: int = int(jz['version'])
+        self.created: datetime = datetime.strptime(jz['create_timestamp'],"%Y-%m-%dT%H:%M:%S%z")
+        self.updated: datetime = datetime.strptime(jz['timestamp'],"%Y-%m-%dT%H:%M:%S%z")
         self.title: str = jz['title']
         self.text: str = jz['text']
+        self.auxiliary_text: str = jz['auxiliary_text']
+        self.headings: List[str] = jz['heading']
+        self.categories: List[str] = jz['category']
+        self.places: List[Place] = [ Place(coord) for coord in jz.get('coordinates', [])]
+        self.outgoing_links: List[str] = jz['outgoing_link']
+        self.count_incoming_links: int = int(jz.get('incoming_links', 0))
+        self.external_links: List[str] = jz['external_link']
+        self.redirects: List[str] = [ rd['title'] for rd in jz.get('redirect',[]) ]
+        # optional properties
+        self.opening_text: Optional[str] = jz.get('opening_text', None) # crawler-style summary
+        self.popularity_score: Optional[float] = jz.get('popularity_score', None)
         self.wikibase_item: Optional[str] = jz.get('wikibase_item', None)
 
 
@@ -18,45 +56,27 @@ class WikiEngine:
     def __init__(self, jsonpath: str):
         self.jsonpath = jsonpath 
         self.h = open(jsonpath, 'r')
+        self.q = 0 # number of articles generated
+        self.err = 0 # number of errors encountered
 
-    def next(self) -> Article:
+    def generate(self) -> Article:
+        # create a generator to get all the wikipedia articles
         while True:
             row = self.h.readline()
             if not row:
-                yield None 
-            jz = row_json(row)
-            if not 'title' in jz:
+                yield None # EOF
+            try:
+                jz = row_json(row)
+            except:
+                self.err += 1
+                print('  WARNING: WikiEngine err # {:,} skipping this non-json row: .'.format(self.err))
+                print(row)
                 continue 
-            yield Article(jz)
-            
-
-if __name__ == '__main__':
-    from sys import argv
-    key_ct, q, err = {}, 0, 0
-    texts = []
-    docs = []
-    rows = []
-    file = argv[1] # 'enwiki-20230206-cirrussearch-content.json' etc.
-    print('Reading', file)
-    h = open(file, 'r')
-    while True:
-        row = h.readline()
-        rows.append(row)
-        if not row:
-            break       # stop after the last line 
-        q += 1          # increment number of rows processed
-        try: 
-            jz = row_json(row)
-        except Exception as e:
-            err += 1 
-            print('Error {} with row: {}'.format(e, row) )
-            continue
-        if 'text' in jz:
-            texts.append(jz['text'])
-            docs.append(jz)
-        for k in jz.keys():
-            key_ct[k] = key_ct.get(k, 0) + 1 
-        if q % 1000 == 0:
-            print('  Processed {:,} rows with {:,} errors  '.format(q, err), end='\r') 
-
-
+            if not 'title' in jz:
+                continue # only every other row is an article document
+            try:
+                a = Article(jz)
+            except Exception as e:
+                print('  WARNING: WikiEngin could not parse an article. Error="{}"'.format(e))
+                continue 
+            yield a
